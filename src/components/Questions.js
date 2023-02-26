cfa6ac5164c30c8184e9630f5399578979a17931
Aswin K <aswinkarat@gmail.com>
2023-02-26 11:55:07 +0530

    added questions to data.js

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import data from "../database/data";
 
 export default function Questions() {
   const [checked, setChecked] = useState(undefined);
+
+  const question = data[0];
+
+  useEffect(() => {
+    console.log(data);
+  });
 
   function onSelect() {
     console.log("radio btn change");
@@ -9,22 +16,24 @@ export default function Questions() {
 
   return (
     <div className="questions">
-      <h1 className="text-light">Qstn 1</h1>
+      <h1 className="text-light">{question.question}</h1>
 
-      <ul>
-        <li>
-          <input
-            type="radio"
-            value={false}
-            name="options"
-            id="q1-option"
-            onChange={onSelect()}
-          />
-          <label className="text-primary" htmlFor="q1-option">
-            option
-          </label>
-          <div className="check checked"></div>
-        </li>
+      <ul key={question.id}>
+        {question.options.map((q, i) => (
+          <li key={i}>
+            <input
+              type="radio"
+              value={false}
+              name="options"
+              id={`q${i}-option`}
+              onChange={onSelect()}
+            />
+            <label className="text-primary" htmlFor={`q${i}-option`}>
+              {q}
+            </label>
+            <div className="check"></div>
+          </li>
+        ))}
       </ul>
     </div>
   );
---
+++ src/database/data.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    id: 1,
+    question: "aaa",
+    options: ["a", "b", "c"],
+  },
+  {
+    id: 2,
+    question: "bbbb",
+    options: ["a", "b", "c"],
+  },
+  {
+    id: 3,
+    question: "cccc",
+    options: ["a", "b", "c"],
+  },
+  {
+    id: 4,
+    question: "dddd",
+    options: ["a", "b", "c"],
+  },
+  {
+    id: 5,
+    question: "eeee",
+    options: ["a", "b", "c"],
+  },
+];
